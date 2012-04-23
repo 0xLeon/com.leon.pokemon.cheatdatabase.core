e42b995ebe6091c86dd405bd405af3391508a62a
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-04-23 09:38:14 +0200

    added DatabaseObject and DatabaseObjectList implementation for cheat database entries

---
+++ files/lib/data/cheatDatabase/entry/Entry.class.php
@@ -0,0 +1,40 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/message/Message.class.php');
+
+/**
+ * Represents a cheat database entry.
+ *
+ * @author	Stefan Hahn
+ * @copyright	2012 Stefan Hahn
+ * @license	Simplified BSD License License <http://projects.swallow-all-lies.com/licenses/simplified-bsd-license.txt>
+ * @package	com.leon.cheatdatabase.core
+ * @subpackage	data.cheatDatabase.entry
+ * @category 	Cheat Database
+ */
+class Entry extends Message {
+	/**
+	 * Creates a new Entry object.
+	 * 
+	 * @param	integer		$entryID
+	 * @param	array		$row
+	 */
+	public function __construct($entryID, $row = null) {
+		if ($entryID !== null) {
+			$sql = "SELECT		entry.*,
+						language.languageItemValue AS name
+				FROM		wcf".WCF_N."_cheat_database_entry entry
+				LEFT JOIN	wcf".WCF_N."_language_item language
+				ON		(language.languageItem = CONCAT('wcf.cheatDatabase.entry.pokemon.', entry.pokedexNumber))
+						AND (language.languageID = ".WCF::getUser()->languageID."
+				WHERE		entry.entryID = ".$entryID;
+			$row = WCF::getDB()->getFirstRow($sql);
+		}
+		$this->messageID = $row['entryID'];
+		parent::__construct($row);
+	}
+	
+	public function getSpritePath() {
+		
+	}
+}
---
+++ files/lib/data/cheatDatabase/entry/EntryList.class.php
@@ -0,0 +1,65 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/DatabaseObjectList.class.php');
+
+// cheat databse imports
+require_once(WCF_DIR.'lib/data/cheatDatabase/entry/Entry.class.php');
+
+/**
+ * Represents a list of cheat database entries.
+ * 
+ * @author	Stefan Hahn
+ * @copyright	2012 Stefan Hahn
+ * @license	Simplified BSD License License <http://projects.swallow-all-lies.com/licenses/simplified-bsd-license.txt>
+ * @package	com.leon.cheatdatabase.core
+ * @subpackage	data.cheatDatabase.entry
+ * @category 	Cheat Database
+ */
+class EntryList extends DatabaseObjectList {
+	/**
+	 * list of entries
+	 * 
+	 * @var array<Entry>
+	 */
+	public $entries = array();
+	
+	/**
+	 * @see DatabaseObjectList::countObjects()
+	 */
+	public function countObjects() {
+		$sql = "SELECT	COUNT(*) AS count
+			FROM	wcf".WCF_N."_cheat_database_entry entry
+			".(!empty($this->sqlConditions) ? "WHERE ".$this->sqlConditions : '');
+		$row = WCF::getDB()->getFirstRow($sql);
+		
+		return $row['count'];
+	}
+	
+	/**
+	 * @see DatabaseObjectList::readObjects()
+	 */
+	public function readObjects() {
+		$sql = "SELECT		".(!empty($this->sqlSelects) ? $this->sqlSelects.',' : '')."
+					entry.*,
+					language.languageItemValue AS name
+			FROM		wcf".WCF_N."_cheat_database_entry entry
+			LEFT JOIN	wcf".WCF_N."_language_item language
+			ON		(language.languageItem = CONCAT('wcf.cheatDatabase.entry.pokemon.', entry.pokedexNumber))
+					AND (language.languageID = ".WCF::getUser()->languageID."
+			".$this->sqlJoins."
+			".(!empty($this->sqlConditions) ? "WHERE ".$this->sqlConditions : '')."
+			".(!empty($this->sqlOrderBy) ? "ORDER BY ".$this->sqlOrderBy : '');
+		$result = WCF::getDB()->sendQuery($sql, $this->sqlLimit, $this->sqlOffset);
+		
+		while ($row = WCF::getDB()->fetchArray($result)) {
+			$this->entries[] = new Entry(null, $row);
+		}
+	}
+	
+	/**
+	 * @see DatabaseObjectList::getObjects()
+	 */
+	public function getObjects() {
+		return $this->entries;
+	}
+}
