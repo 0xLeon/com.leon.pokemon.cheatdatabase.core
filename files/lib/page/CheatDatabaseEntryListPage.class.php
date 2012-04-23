16a33ad6cecccdf3a33cf68ea5b78889499461d5
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-04-23 09:38:48 +0200

    added page class for cheat database entry list page

---
+++ files/lib/page/CheatDatabaseEntryListPage.class.php
@@ -0,0 +1,111 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/SortablePage.class.php');
+require_once(WCF_DIR.'lib/page/util/menu/PageMenu.class.php');
+
+// cheat database imports
+require_once(WCF_DIR.'lib/data/cheatDatabase/entry/EntryList.class.php');
+
+/**
+ * Shows a list of all cheat database entries.
+ *
+ * @author	Stefan Hahn
+ * @copyright	2012 Stefan Hahn
+ * @license	Simplified BSD License License <http://projects.swallow-all-lies.com/licenses/simplified-bsd-license.txt>
+ * @package	com.leon.cheatdatabase.core
+ * @subpackage	page
+ * @category 	Cheat Database
+ */
+class CheatDatabaseEntryListPage extends SortablePage {
+	public $templateName = 'cheatDatabaseEntryList';
+	public $defaultSortField = 'entry.pokedexNumber';
+	public $defaultSortOrder = 'ASC';
+	public $deletedEntryID = 0;
+	
+	/**
+	 * Entry list object
+	 * 
+	 * @var	EntryList
+	 */
+	public $entryList = null;
+	
+	/**
+	 * @see Page::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (isset($_REQUEST['deletedEntryID'])) $this->deletedEntryID = intval($_REQUEST['deletedEntryID']);
+		$this->entryList = new EntryList();
+	}
+	
+	/**
+	 * @see Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		$this->entryList->sqlOffset = ($this->pageNo - 1) * $this->itemsPerPage;
+		$this->entryList->sqlLimit = $this->itemsPerPage;
+		$this->entryList->sqlOrderBy = $this->sortField." ".$this->sortOrder;
+		$this->entryList->readObjects();
+	}
+	
+	/**
+	 * @see SortablePage::validateSortField()
+	 */
+	public function validateSortField() {
+		parent::validateSortField();
+		
+		switch ($this->sortField) {
+			case 'pokemon':
+				$this->sortField = 'name';
+				break;
+			case 'nickname':
+			case 'trainerName':
+			case 'trainerID':
+			case 'level':
+				$this->sortField = 'entry.'.$this->sortField;
+				break;
+			case 'number':
+			default:
+				$this->sortField = $this->defaultSortField;
+		}
+	}
+	
+	/**
+	 * @see MultipleLinkPage::countItems()
+	 */
+	public function countItems() {
+		parent::countItems();
+		
+		return $this->entryList->countObjects();
+	}
+	
+	/**
+	 * @see Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'entries' => $this->entryList->getObjects(),
+			'deletedEntryID' => $this->deletedEntryID
+		));
+	}
+	
+	/**
+	 * @see Page::show()
+	 */
+	public function show() {
+		if (!MODULE_CHEAT_DATABASE) {
+			throw new IllegalLinkException();
+		}
+		
+		PageMenu::setActiveMenuItem('wcf.header.menu.cheatDatabase');
+		
+		WCF::getUser()->checkPermission(array());
+		
+		parent::show();
+	}
+}
