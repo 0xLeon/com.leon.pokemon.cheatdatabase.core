de709da19882c71c1d135e7adf49052ad7e221ab
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-04-27 08:43:59 +0200

    removed inserts used for debugging

--- install.sql
+++ install.sql
@@ -43,25 +43,3 @@ CREATE TABLE wcf1_cheat_database_entry_message (
 	PRIMARY KEY (messageID),
 	FULLTEXT KEY (subject, message)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO wcf1_cheat_database_entry (entryID, messageID, pokedexNumber, form, nickname, gender, trainerName, trainerID, level, catchLocation, catchDate, catchLevel, nature, characteristic, isShiny, ball) VALUES
-(1, 1, 6, 0, 'Dragon', 1, 'Leon', 56789, 100, 'Dort', 111111111, 10, 5, 6, 1, 23),
-(2, 2, 1, 0, 'Bisersam', 2, 'Robert Robert Agular', 1, 1, 'Hier', 111111111, 1, 1, 1, 0, 1),
-(3, 3, 124, 0, 'Kyons Mutter', 0, 'Leon', 56789, 100, 'Kyons Zuhause.', 111111111, 1, 1, 1, 0, 12),
-(4, 4, 251, 0, '', 0, 'Cheater', 0, 100, 'Schicksalhafte Begegnung', 255114841, 10, 5, 9, 1, 4),
-(5, 5, 351, 1, '', 1, 'Leon', 56789, 50, 'Klimainstitut', 2551148416, 50, 1, 1, 0, 1),
-(6, 6, 386, 2, '', 0, 'Leon', 56789, 86, 'Schicksalhafte Begegnung', 2551148416, 1, 10, 10, 1, 10);
-
-INSERT INTO wcf1_cheat_database_ribbon_to_entry (entryID, ribbonID) VALUES
-(1, 68),
-(1, 78),
-(1, 45),
-(3, 53);
-
-INSERT INTO wcf1_cheat_database_entry_message (messageID, entryID, userID, username, subject, message, time, ipAddress, enableSmilies, enableHtml, enableBBCodes) VALUES
-(1, 1, 1, 'Leon', 'Pokesav Shiny Glurak', 'Voll Cheat, digga.', 222222222, '127.0.0.1', 1, 0, 1),
-(2, 2, 1, 'Leon', 'Versager Bisasam', 'Krass Cheat.', 222222222, '127.0.0.1', 1, 0, 1),
-(3, 3, 1, 'Leon', 'Kyons fette Mutter', 'Zu fett.', 222222222, '127.0.0.1', 1, 0, 1),
-(4, 4, 1, 'Leon', 'Shiny Colosseum Celebi', '[b]Cheat[/b]', 255914841, '127.0.0.1', 0, 0, 1),
-(5, 5, 1, 'Leon', 'Cheat Formeo', '', 2951148416, '127.0.0.1', 1, 0, 1),
-(6, 6, 1, 'Leon', 'Cheat Shiny Deoxys', '', 2951148416, '127.0.0.1', 1, 0, 1);
